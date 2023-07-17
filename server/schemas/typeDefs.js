c4e179ab91e63fd5bb7683bdb0b6d63e87c0cbb3
Ryan Golder <rgolder78@gmail.com>
2023-07-17 21:22:06 +1000

    Code typeDefs.js file

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    reviews: [Review]!
+}
+
+type Review {
+    _id: ID
+    reviewText: String
+    reviewAuthor: String
+    createdAt: String
+    bike: String
+    rating: Int
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+
+type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    reviews(username: String): [Review]
+    review(_id: ID!): Review
+}
+
+type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    addReview(reviewText: String!, bike: String!, rating: Int!): Review
+    removeReview(reviewId: ID!): Review
+}
+`;
+
+module.exports = typeDefs;
