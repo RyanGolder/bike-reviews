d0762dd8cdca9957dbab022950bd2f9b20e66d57
Ryan Golder <rgolder78@gmail.com>
2023-07-18 14:39:18 +1000

    Code the seed.js file within the seeds folder on server side

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,21 +1,31 @@
-const db = require('../config/connection');
-const { User, Review } = require('../models');
-const userSeeds = require('./userSeeds.json');
-const reviewSeeds = require('./reviewSeeds.json');
+const db = require("../config/connection");
+const { User, Review } = require("../models");
+const userSeeds = require("./userSeeds.json");
+const reviewSeeds = require("./reviewSeeds.json");
 
-// db.once('open', async () => {
-//     try {
-//         await User.deleteMany({});
-//         await Review.deleteMany({});
+db.once("open", async () => {
+  try {
+    await Review.deleteMany({});
+    await User.deleteMany({});
 
-//         await User.create(userSeeds);
-//         await Review.create(reviewSeeds);
+    await User.create(userSeeds);
 
-//         console.log('all done!');
-//         process.exit(0);
-//     } catch (err) {
-//         throw err;
-//     }
-// }
-// );
+    for (let i = 0; i < reviewSeeds.length; i++) {
+      const { _id, reviewAuthor } = await Review.create(reviewSeeds[i]);
+      const user = await User.findOneAndUpdate(
+        { username: reviewAuthor },
+        {
+          $addToSet: {
+            reviews: _id,
+          },
+        }
+      );
+    }
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 
+  console.log("all done!");
+  process.exit(0);
+});
